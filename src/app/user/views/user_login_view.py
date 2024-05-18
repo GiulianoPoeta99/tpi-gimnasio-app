59dd8dfd0248f8954db576047a5f81ee0e77fb19
Lk <lucasaraya19@hotmail.com>
2024-05-18 18:27:53 -0300

    fix(backend): :bug: Se corrigio error de login
    No se podia acceder con el email

--- src/app/user/views/user_login_view.py
+++ src/app/user/views/user_login_view.py
@@ -1,15 +1,31 @@
-# app/user/views/user_login_view.py
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 class UserLoginView(LoginView):
     template_name = 'user/login.html'
-    
+
     def get_success_url(self):
         return reverse_lazy('construction')
 
     def form_invalid(self, form):
-        messages.error(self.request, 'Invalid username or password.')
+        # No agregar el mensaje aquí para evitar duplicación
         return super().form_invalid(form)
+
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect(self.get_success_url())
+        else:
+            messages.error(request, 'Invalid email or password.', extra_tags='error')
+            return self.form_invalid(self.get_form())
+
+    def get_form(self):
+        return super().get_form()
