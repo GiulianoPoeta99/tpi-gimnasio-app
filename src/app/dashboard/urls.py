c296d98b25ef1292a490741c45e9e23167c27bbb
lucas <lucasaraya19@hotmail.com>
2024-05-29 20:57:02 -0300

    refactor(backend): :recycle: Cambios en dashboard

--- src/app/dashboard/urls.py
+++ src/app/dashboard/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path , include
 from app.dashboard import views
 
 urlpatterns = [
     path('', views.index, name='dashboard'),
+    path('trainers/', include('app.trainer.urls')),
 ]
