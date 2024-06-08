73f7177548e92384ae797ebebab5920c5bf8b2ff
Lk <lucasaraya19@hotmail.com>
2024-06-08 19:38:56 -0300

    feat(backend): :recycle: se arreglo crud trainer y se cambio logo

--- src/app/trainer/form.py
+++ src/app/trainer/form.py
@@ -1,11 +1,38 @@
 from django import forms
+from app.user.model import User
 from app.trainer.model import Trainer
 
 class TrainerForm(forms.ModelForm):
+    user = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        label='Usuario',
+        required=True,
+        widget=forms.Select(attrs={
+            'class': 'form-select form-control-lg',
+            'id': 'user'
+        })
+    )
+
+    is_verify = forms.BooleanField(
+        label='Verificado',
+        required=False,
+        widget=forms.CheckboxInput(attrs={
+            'class': 'form-check-input',
+            'id': 'is_verify'
+        })
+    )
+
+    RATE_CHOICES = [(i, i) for i in range(1, 6)]  # Opciones de 1 a 5
+
+    rate = forms.ChoiceField(
+        label='Calificación',
+        choices=RATE_CHOICES,
+        widget=forms.Select(attrs={
+            'class': 'form-select form-control-lg',
+            'id': 'rate'
+        })
+    )
+
     class Meta:
         model = Trainer
         fields = ['user', 'is_verify', 'rate']
-        widgets = {
-            'is_verify': forms.CheckboxInput(),
-            'rate': forms.NumberInput(attrs={'min': 1, 'max': 5}),
-        }
