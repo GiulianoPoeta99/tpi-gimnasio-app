c4492d2ec7779b87f91ae26176892b9f94f8482e
coty <cotybenedetti5@gmail.com>
2024-07-03 12:11:21 -0300

    feat: :bug: se arreglo el campo many to many de trainer

--- src/app/trainer/form.py
+++ src/app/trainer/form.py
@@ -1,6 +1,6 @@
 from django import forms
-from app.user.model import User
-from app.trainer.model import Trainer
+from app.user.model import User  
+from app.trainer.model import Trainer 
 
 class TrainerForm(forms.ModelForm):
     user = forms.ModelChoiceField(
@@ -22,7 +22,7 @@ class TrainerForm(forms.ModelForm):
         })
     )
 
-    RATE_CHOICES = [(i, i) for i in range(1, 6)]  # Opciones de 1 a 5
+    RATE_CHOICES = [(i, i) for i in range(1, 6)]  
 
     rate = forms.ChoiceField(
         label='Calificación',
@@ -33,6 +33,15 @@ class TrainerForm(forms.ModelForm):
         })
     )
 
+    users = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        label='Usuarios',
+        required=False,
+        widget=forms.CheckboxSelectMultiple
+    )
+
+
     class Meta:
         model = Trainer
-        fields = ['user', 'is_verify', 'rate']
+        fields = ['user', 'is_verify', 'rate', 'users']
+
